9c3a126a896ff6814cc67367def26b549cb0d20b
alizapahmer <38965000+alizapahmer@users.noreply.github.com>
2020-12-30 17:26:19 +0200

    Update app.js

--- app.js
+++ app.js
@@ -2,40 +2,40 @@ const express = require('express');
 const mongoose= require('mongoose');
 const app= express();
 const port=8080;
-const geolocationRoutes = require('./routes/geolocationRoutes') 
+const geolocationRoutes = require('./routes/geolocationRoutes')
+const healthCheckMongodb = require('health-check-mongodb'); 
 app.use(express.urlencoded({extended: true}));
 mongoose.set('useFindAndModify', false);
 app.set('view engine', 'ejs');
 
+
 //connection link to database
-const dbURI = 'mongodb+srv://aliza_aron:<password>@cluster0.tt8n8.mongodb.net/geolocation?retryWrites=true&w=majority';
+const dbURI = 'mongodb+srv://aliza_aron:ALpa3006@cluster0.tt8n8.mongodb.net/geolocation?retryWrites=true&w=majority';
 
 //connect to Database
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology:true})
 	.then((result)=>console.log("connected to db"))
-	.catch((err)=>console.log("error! am not connected"));
+	.catch((err)=>console.log("Warning Database is not connected"));
 
 app.listen(port);
 
 //geolocation routes
 app.use(geolocationRoutes)
 
-app.get('/hello', (req, res) => {
-    res.send(res.statusCode)
-});
-
 
 //API #3, health API, uses npm health check to monitor 
 app.get('/health',(req,res)=>{
-healthCheckMongodb.do({
-   url:dbURI
- })
-   .then(response => {
-     if (response.health == false){
-        console.log("got here") 
-        res.status(500) 
-        res.send(500) 
-     }
-     res.end() 
-   })
+  healthCheckMongodb.do({
+    url:dbURI
+  })
+  .then(response => {
+    if (response.health == false){ 
+      res.status(500) 
+      res.send(res.statusCode) 
+    }
+    else{
+    res.status(200)
+    res.send(res.statusCode) 
+    }
+  })
 })
